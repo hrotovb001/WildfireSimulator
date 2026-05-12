6e534c648acb29061590b37a9558b741c569b882
Bohdan Hrotovytskyy <bohdanhrotovytskyy@gmail.com>
2026-05-11 21:14:10 -0700

    chore: remove unecessary conftest.py

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pathlib import Path
-
-# Add the src directory to sys.path so that 'wildfire_simulator' can be imported
-src_path = Path(__file__).resolve().parent.parent / "src"
-if src_path.exists():
-    sys.path.insert(0, str(src_path))
