ab8a2e77d2ca1da77d3fdc9d3680ec794a6910d2
Bohdan Hrotovytskyy <bohdanhrotovytskyy@gmail.com>
2026-06-18 18:35:50 -0700

    test: make simulator behaviour conform to model expectations

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,11 +1,13 @@
 import torch
 import pytest
 import copy
+import numpy as np
 
 from wildfire_simulator.simulators import ForwardBurnSimulator, fire_burn_step
+from wildfire_simulator.transforms import MinMaxPerChannel
 
 def test_burn_step():
-    inputs = torch.zeros((1, 14, 512, 512))
+    inputs = torch.zeros((1, 13, 500, 500))
     inputs_copy = copy.deepcopy(inputs)
     def model(data):
         chan1 = torch.full((1, 1, 512, 512), 1.0)
@@ -13,6 +15,7 @@ def test_burn_step():
         return [torch.cat((chan1, chan2), dim=1)]
     outputs = fire_burn_step(0, model, inputs)
     assert (inputs == inputs_copy).all()
+    assert outputs.shape == (1, 13, 500, 500)
     assert (outputs[0][0] == 1.0).all()
     assert (outputs[0][1] == 2.0).all()
 
@@ -21,7 +24,7 @@ def test_simulator():
     def model(data):
         return data * 2
 
-    def step(t, data, model):
+    def step(t, model, data):
         return model(t + data)
 
     class FakeTransform:
@@ -36,6 +39,7 @@ def test_simulator():
     transform = FakeTransform()
 
     simulator = ForwardBurnSimulator(
+        t0=4,
         data=10,
         model=model,
         step=step,
@@ -44,6 +48,28 @@ def test_simulator():
         max_t=20
     )
 
-    assert simulator.run_to(10) == pytest.approx(207.6)
-    assert simulator.run_to(10, return_history=True) == pytest.approx([10, 12, 24.6, 50.4, 102.6, 207.6])
+    assert simulator.run_to(14) == pytest.approx(244.8)
+    assert simulator.run_to(14, return_history=True) == pytest.approx([10, 13.2, 28.2, 58.8, 120.6, 244.8])
+
+
+def test_simulator_integration():
+    inputs = torch.zeros((1, 13, 500, 500))
+
+    def model(data):
+        return torch.full((1, 2, 512, 512), 2.0)
+
+    transform = MinMaxPerChannel(np.full((13,), 0.0), np.full((13,), 2.0))
+
+    simulator = ForwardBurnSimulator(
+        data=inputs,
+        model=model,
+        step=fire_burn_step,
+        transform=transform,
+        dt=1,
+        max_t=2
+    )
+
+    output = simulator.run_to(2)
+
+    assert (output[:, :2] == 4).all()
 
