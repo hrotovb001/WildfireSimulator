f85fb40334cbdedf4de8034ca1da6934ffc2e236
Bohdan Hrotovytskyy <bohdanhrotovytskyy@gmail.com>
2026-06-20 17:14:25 -0700

    test: improve image utility tests by comparing against baseline imgs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,12 @@
 import numpy as np
 from pathlib import Path
 import shutil
+from PIL import Image
 
 from wildfire_simulator.utils import save_frame, save_comparison
 
 def test_save_frame():
-    rng = np.random.default_rng()
+    rng = np.random.default_rng(seed=42)
     data = rng.random((13, 500, 500))
     
     shutil.rmtree('./tmp', ignore_errors=True)
@@ -15,12 +16,20 @@ def test_save_frame():
     file_path = Path('./tmp/random_frame.png')
     assert file_path.is_file()
 
-    size_bytes = file_path.stat().st_size
-    assert size_bytes > 1000
+    img1 = Image.open('./tmp/random_frame.png')
+    img2 = Image.open('./tests/baseline/random_frame.png')
+
+    assert img1.mode == img2.mode
+
+    arr1 = np.asarray(img1)
+    arr2 = np.asarray(img2)
+
+    assert arr1.shape == arr2.shape
+    assert np.array_equal(arr1, arr2)
 
 
 def test_save_comparison():
-    rng = np.random.default_rng()
+    rng = np.random.default_rng(seed=42)
 
     # 10 different fires where each image is an arrival map
     trues = rng.random((10, 500, 500))
@@ -33,6 +42,14 @@ def test_save_comparison():
     file_path = Path('./tmp/random_comparison.png')
     assert file_path.is_file()
 
-    size_bytes = file_path.stat().st_size
-    assert size_bytes > 1000
-    
+    img1 = Image.open('./tmp/random_comparison.png')
+    img2 = Image.open('./tests/baseline/random_comparison.png')
+
+    assert img1.mode == img2.mode
+
+    arr1 = np.asarray(img1)
+    arr2 = np.asarray(img2)
+
+    assert arr1.shape == arr2.shape
+    assert np.array_equal(arr1, arr2)
+
